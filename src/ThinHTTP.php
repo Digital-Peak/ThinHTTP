c80b9b83210630ec0b13ee19f46bdf1c79a14ef7
Allon Moritz <allon.moritz@digital-peak.com>
2022-07-01 16:26:08 +0200

    Follow the location on requests

--- src/ThinHTTP.php
+++ src/ThinHTTP.php
@@ -72,6 +72,7 @@ class ThinHTTP implements ThinHTTPInterface
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, strtoupper($method));
 		curl_setopt($ch, CURLOPT_USERAGENT, 'DPCalendar');
 
